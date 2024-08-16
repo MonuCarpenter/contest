7d3f676e603d5b0b93eefd11d26450885c1d7a9b
Monu Carpenter <monu.carpenter@coralmango.com>
2024-08-16 21:11:32 +0530

    WIP: 2024-08-16 21:11:32

---
+++ 1878/c/code.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author: Monu Carpenter
+ * Time: 2024-08-16 21:11:27
+ * Handle: m_o_n_u
+**/
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+void solve() {
+
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int T; std::cin >> T; 
+
+    while(T--)
+     solve();
+    return 0;
+}
