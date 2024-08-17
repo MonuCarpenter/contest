370eb62867267dbfaf97bf57f273ba806d355b00
Monu Carpenter <monu.carpenter@coralmango.com>
2024-08-17 19:30:40 +0530

    WIP: 2024-08-17 19:30:40

--- 1948/a/code.cpp
+++ 1948/a/code.cpp
@@ -2,24 +2,35 @@
  * Author: Monu Carpenter
  * Time: 2024-08-17 19:22:14
  * Handle: m_o_n_u
-**/
+ **/
 
 #include <bits/stdc++.h>
 
 typedef long long ll;
 
 void solve() {
-
+    int n;
+    std:: cin >> n;
+    if (n % 2 == 1) {
+       st cout << "NO" << '\n';
+        continue;
+    }
+    cout << "YES" << '\n';
+    for (int i = 0; i < n / 2; ++i)
+        for (int j = 0; j < 2; ++j)
+            cout << "AB"[i & 1];
+    cout << '\n';
 }
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    std::cout.tie(nullptr); 
-        
-    int T; std::cin >> T; 
+    std::cout.tie(nullptr);
 
-    while(T--)
-     solve();
+    int T;
+    std::cin >> T;
+
+    while (T--)
+        solve();
     return 0;
 }
--- run.sh
+++ run.sh
@@ -46,7 +46,7 @@ void solve() {
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(0);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr); 
         
