6d611972153245bd81d60fa043718fff98d7a7fd
Monu Carpenter <monu.carpenter@coralmango.com>
2024-08-17 11:31:01 +0530

    WIP: 2024-08-17 11:31:01

--- 1878/c/code.cpp
+++ 1878/c/code.cpp
@@ -13,7 +13,7 @@ void solve() {
     std ::cin >> n >> k >> x;
 
     ll min_sum = x * (x + 1) / 2;
-    ll max_sum = n * (n + 1) - (n - x) * (n - x - 1);
+    ll max_sum = n * (n + 1) - (n - x) * (n - x + 1);
 
     if (k >= min_sum && k <= max_sum) {
         std::cout << "YES\n";
