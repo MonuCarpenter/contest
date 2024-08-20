691bdb85d642d53be30225aa2333dbb74515dda7
Monu Carpenter <monucarpenter@Monus-Mac-mini.local>
2024-08-20 20:38:33 +0530

    WIP: 2024-08-20 20:38:33

---
+++ 2001/b/code.cpp
@@ -0,0 +1,25 @@
+/*
+ * Author: Monu Carpenter
+ * Time: 2024-08-20 20:38:32
+ * Handle: m_o_n_u
+**/
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+void solve() {
+
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr); 
+        
+    int T; std::cin >> T; 
+
+    while(T--)
+     solve();
+    return 0;
+}
