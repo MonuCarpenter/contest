8186c71a32405523894e29317ff2c6aa9e0be270
Monu Carpenter <monucarpenter@Monus-Mac-mini.local>
2024-09-10 17:26:29 +0530

    wip

--- codeforces/1476/a/code.cpp
+++ codeforces/1476/a/code.cpp
@@ -1,28 +1,31 @@
 /*
  * Author: Monu Carpenter
  * Handle: m_o_n_u
- * Time: 2024-09-08 20:18:22
- * Problem: 479/a
-**/
+ * Time: 1725961541
+ * Problem: 1476/a
+ */
 
 #include <bits/stdc++.h>
 #define _for(n) for (int i = 0; i < n; i++)
-
 typedef long long ll;
 
 void solve() {
+    int n, k;
+    std::cin >> n >> k;
 
+    int remainder = (k - (n % k)) % k;
+
+    int result = 1 + (remainder + n - 1) / n;
+
+    std::cout << result << '\n';
 }
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
- 
     int T;
     std::cin >> T;
-
     while (T--)
         solve();
-
     return 0;
 }
