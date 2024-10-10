2936af8c0fb70118c464ce32c7cfb358b8b33ca6
Monu Carpenter <monuc1315@gmail.com>
2024-10-10 12:54:03 +0530

    refactor(chat bot): deleted toggle chat

--- template.cpp
+++ template.cpp
@@ -9,10 +9,27 @@ using i64 = long long;
 using u64 = unsigned long long;
 using u32 = unsigned;
 
+#ifndef ONLINE_JUDGE
+#include "../cpp-dump.hpp"
+#define dump(...) cpp_dump(__VA_ARGS__)
+template <>
+inline void cpp_dump::write_log(std::string_view output) {
+    std::cout << out/put << '\n';
+}
+#else
+#define dump(...)
+#define CPP_DUMP_SET_OPTION(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM_GENERIC(...)
+#endif
+
 void solve() {
 }
 
 int main() {
+    CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
