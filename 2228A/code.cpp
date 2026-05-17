6da8f14dc3ef669f7c8d9e61cf5bbaf14402bc51
Monu Carpenter <monuc1315@gmail.com>
2026-05-17 13:49:00 +0530

    added: codeforces solutions summing-digits

---
+++ 2228A/code.cpp
@@ -0,0 +1,71 @@
+/*
+ * Author: Monu Carpenter
+ * Handle: m_o_n_u
+ * Time: 01:19 PM IST
+ * Problem: <problem-name>
+ */
+
+#include <bits/stdc++.h>
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+
+#ifndef ONLINE_JUDGE
+#include "../cpp-dump/cpp-dump.hpp"
+#define log(...) cpp_dump(__VA_ARGS__)
+template <> inline void cpp_dump::write_log(std::string_view output) {
+  std::cout << output << '\n';
+}
+#else
+#define log(...)
+#define CPP_DUMP_SET_OPTION(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM_GENERIC(...)
+#endif
+
+int solve() {
+  int n;
+  std::cin >> n;
+
+  int zeros = 0, ones = 0, twos = 0;
+
+  while (n--) {
+    int x;
+    std::cin >> x;
+
+    if (x == 0)
+      zeros++;
+    else if (x == 1)
+      ones++;
+    else if (x == 2)
+      twos++;
+  }
+
+  int result = 0;
+
+  result += zeros;
+  result += std::min(ones, twos);
+  result += (twos - std::min(ones, twos)) / 3;
+  result += (ones - std::min(ones, twos)) / 3;
+
+  std::cout << result << "\n";
+  return 0;
+}
+
+int main() {
+  CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int t;
+  std::cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
