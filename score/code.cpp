d1ccc2908734c2c5cf1d40d39b4a94dbed19ceab
Monu Carpenter <monuc1315@gmail.com>
2026-05-18 19:00:59 +0530

    added: codeforces solutions sub_prime

---
+++ score/code.cpp
@@ -0,0 +1,68 @@
+/*
+ * Author: Monu Carpenter
+ * Handle: m_o_n_u
+ * Time: 06:08 PM IST
+ * Problem: <problem-name>
+ */
+
+#include <bits/stdc++.h>
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+//
+// #ifndef ONLINE_JUDGE
+// #include "../cpp-dump/cpp-dump.hpp"
+// #define log(...) cpp_dump(__VA_ARGS__)
+// template <> inline void cpp_dump::write_log(std::string_view output) {
+//   std::cout << output << '\n';
+// }
+// #else
+// #define log(...)
+// #define CPP_DUMP_SET_OPTION(...)
+// #define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+// #define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
+// #define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+// #define CPP_DUMP_DEFINE_EXPORT_ENUM_GENERIC(...)
+// #endif
+
+class Solution {
+public:
+  int calculate_score(std::string s) {
+    // log(s);
+    int start = 0;
+    int end = 1;
+
+    int result = 0;
+    while (start < s.size()) {
+      // log(start, end, result);
+      if (s[start] == 'O') {
+        result += end;
+        end++;
+      } else {
+        end = 1;
+      }
+      start++;
+    }
+    return result;
+  }
+};
+
+int main() {
+  // CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  Solution solution;
+
+  int t;
+  std::cin >> t;
+
+  while (t--) {
+    std::string s;
+    std::cin >> s;
+    std::cout << solution.calculate_score(s) << std::endl;
+  }
+
+  return 0;
+}
