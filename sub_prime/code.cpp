605163d9cbd09a7ceffcfe804211b3ac9fff9dce
Monu Carpenter <monuc1315@gmail.com>
2026-05-19 12:18:39 +0530

    added: codeforces solutions CF2230A

---
+++ sub_prime/code.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: Monu Carpenter
+ * Handle: m_o_n_u
+ * Time: 07:01 PM IST
+ * Problem: <problem-name>
+ */
+
+#include <bits/stdc++.h>
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+
+#ifndef ONLINE_JUDGE
+#include "../cpp-dump/cpp-dump.hpp"
+#define log(...) cpp_dump(__VA_ARGS__)
+template <> inline void cpp_dump::write_log(std::string_view output) {
+  std::cout << output << '\n';
+}
+#else
+#define log(...)
+#define CPP_DUMP_SET_OPTION(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM_GENERIC(...)
+#endif
+
+class Solution {
+public:
+  bool payable(int B, int N) { return true; }
+};
+
+int main() {
+  CPP_DUMP_SET_OPTION(es_style, cpp_dump::types::es_style_t::no_es);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int B, N;
+  std::cin >> B >> N;
+
+  Solution solution;
+
+  while (std::cin >> B >> N) {
+    std::cout << (solution.payable(B, N) ? "S" : "B") << "\n";
+  }
+  return 0;
+}
