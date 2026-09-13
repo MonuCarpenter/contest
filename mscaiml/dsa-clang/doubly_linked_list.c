a49fc7b06810a81bdeeb69539e10774578889929
Monu Carpenter <monuc1315@gmail.com>
2026-09-13 13:02:54 +0530

    added: doubly linked list in c

--- mscaiml/dsa-clang/doubly_linked_list.c
+++ mscaiml/dsa-clang/doubly_linked_list.c
@@ -97,7 +97,6 @@ void insert_at_position(Node **head, int val, int pos) {
 /**
  *  @description: Print List Forward
  * */
-
 void print_list_forward(Node *head) {
   Node *current = head;
 
@@ -112,7 +111,6 @@ void print_list_forward(Node *head) {
 /**
  *  @description: Print List Backward
  * */
-
 void print_list_backward(Node *head) {
   Node *current = head;
 
@@ -131,7 +129,6 @@ void print_list_backward(Node *head) {
 /**
  *  @description: Print list with type
  * */
-
 typedef enum { FORWARD, BACKWARD } Type;
 
 void print_list(Node *head, Type type) {
@@ -142,6 +139,82 @@ void print_list(Node *head, Type type) {
   }
 }
 
+/**
+ *  @description: Delete node at the beginning of the list;
+ * */
+void delete_at_beginning(Node **head) {
+  if (*head == NULL)
+    return;
+
+  Node *current = *head;
+  *head = (*head)->next;
+
+  if (*head != NULL)
+    (*head)->prev = NULL;
+
+  free(current);
+}
+
+/**
+ *  @description: Delete node at the end of the list;
+ * */
+void delete_at_end(Node **head) {
+  if (*head == NULL)
+    return;
+
+  Node *end = *head;
+
+  if (end->next == NULL) {
+    free(end);
+    *head = NULL;
+    return;
+  }
+
+  while (end->next != NULL) {
+    end = end->next;
+  }
+  // we can also do like going to second last
+  // and then delete the next of the second last
+  // that we did in the signly linked list where we use
+  // while(end->next->next != NULL) and then free(end->next)
+  // but in doubly we have one advantage that we can ge
+  // backward; so we are doing end->prev->next that is the end itself;
+  end->prev->next = NULL;
+  free(end);
+}
+
+/**
+ *  @description: Delete node at any position in the list;
+ * */
+void delete_at_position(Node **head, int pos) {
+  if (pos <= 0 || *head == NULL)
+    return;
+
+  if (pos == 1) {
+    delete_at_beginning(head);
+    return;
+  }
+
+  Node *current = *head;
+
+  for (int i = 1; i < pos && current != NULL; i++) {
+    current = current->next;
+  }
+
+  if (current == NULL)
+    return;
+
+  if (current->prev != NULL) {
+    current->prev->next = current->next;
+  }
+
+  if (current->next != NULL) {
+    current->next->prev = current->prev;
+  }
+
+  free(current);
+}
+
 int main() {
   Node *head = NULL;
 
@@ -149,6 +222,13 @@ int main() {
   insert_at_end(&head, 2);
   insert_at_beginning(&head, 0);
   insert_at_position(&head, 3, 4);
+
+  print_list(head, FORWARD);
+  print_list(head, BACKWARD);
+
+  delete_at_position(&head, 2);
+  delete_at_end(&head);
+  delete_at_beginning(&head);
 
   print_list(head, FORWARD);
   print_list(head, BACKWARD);
